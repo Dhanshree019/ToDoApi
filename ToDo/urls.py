e52ee6166c16f962815807bad5f11625f41223e5
Kolwankar-Siddhiraj <connect.siddhiraj@gmail.com>
2024-08-04 23:16:59 +0530

    deployment setup

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -17,7 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/',include('ToDoApi.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
